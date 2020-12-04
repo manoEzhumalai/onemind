e80c661d19f05a625c768063fc1f74f4fe099e3a
ezhumalai <ezhumalai@niyatainfotech.com>
2020-12-04 15:28:31 +0530

    signup  page

--- src/app/layout/Product/AddProduct/AddProduct.component.ts
+++ src/app/layout/Product/AddProduct/AddProduct.component.ts
@@ -15,23 +15,59 @@ export class AddProductComponent {
     availableQuantity: ['', [Validators.required]],
 
   });
-
+  fulldata:any
   loader = true
   constructor( private router: Router ,public service:ApiCallOneService,private fb: FormBuilder) { }
   get f() { return this.profileForm.controls }
 
   ngOnInit(): void {
+    this.getProduct()
 }
 
 submit(){
-  this.loader=true
+  var x=this.profileForm.value
+  var filter= this.fulldata.filter((obj:any)=>obj.productName==x.productName)
+  console.log(filter)
+  if(filter.length==0){
+      this.loader=true
 
-  this.service.addProduct(this.profileForm.value).subscribe((data:any) => {
+ this.service.addProduct(x).subscribe((data:any) => {
     
     this.loader=false
     this.service.showSuccess('Product Added Successfully !!!')
 
     this.router.navigate(['list'])
+
+
+  },error =>{
+    this.loader=false
+    this.service.showError()
+
+  } )
+  }else{
+    this.service.showInfo('Product with same name already available !!!')
+
+  }
+ 
+}
+getProduct(){
+  this.loader=true
+
+  this.service.getProduct().subscribe((data:any) => {
+    this.fulldata=data
+
+    console.log(this.fulldata)
+
+    // this.fulldata=this.fulldata.map((obj:any)=>{
+    //     if(obj.productName==null){
+    //       obj.productName="&&"
+    //     }
+    //     return obj
+    // })
+    // this.productList=this.fulldata
+
+    this.loader=false
+
 
 
   },error =>{
